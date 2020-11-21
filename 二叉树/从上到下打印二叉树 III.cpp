d94fc48b055cf32b32039c91e0ad98c69f24a031
xjq <lebnizjoan@gmail.com>
2020-11-22 03:50:42 +0800

    从上到下打印二叉树 III

--- 二叉树/从上到下打印二叉树 III.cpp
+++ 二叉树/从上到下打印二叉树 III.cpp
@@ -31,3 +31,44 @@ public:
         return v;
     }
 };
+
+// 双端队列
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> v;
+        if(!root)return v;
+        deque<TreeNode*> q;
+        q.push_front(root);
+        bool f=true;
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> temp;
+            for(int i=0;i<size;i++){
+                if(!f){
+                    TreeNode* cur = q.back();q.pop_back();
+                    temp.push_back(cur->val);
+                    if(cur->right)q.push_front(cur->right);  
+                    if(cur->left)q.push_front(cur->left);
+                }else{
+                    TreeNode* cur = q.front();q.pop_front();
+                    temp.push_back(cur->val);
+                    if(cur->left)q.push_back(cur->left);
+                    if(cur->right)q.push_back(cur->right);  
+                }
+            }
+            f=!f;
+            v.push_back(temp);
+        }
+        return v;
+    }
+};
