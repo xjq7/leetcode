802f4eec82a4dc80920069585916be6b5b148e14
xjq <lebnizjoan@gmail.com>
2020-11-23 01:16:59 +0800

    最长上升子序列

---
+++ 动态规划/最长上升子序列.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size()==0)return 0;
+        vector<int> dp(nums.size(),1);
+        int m=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+            m=max(dp[i],m);
+        }           
+        return m;
+    }
+};
