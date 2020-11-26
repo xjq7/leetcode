d566eb8fe0b83236edf450344c2c9dd7e41d6bda
xjq <lebnizjoan@gmail.com>
2020-11-26 15:47:13 +0800

    旋转链表

--- 链表/旋转链表.cpp
+++ 链表/旋转链表.cpp
@@ -8,6 +8,8 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//  利用栈
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
@@ -29,6 +31,43 @@ public:
         }
         ListNode* rear=s.top();
         rear->next=nullptr;
+        return n;
+    }   
+};
+
+// 先连接成环,然后在适当的地方解环
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head)return nullptr;
+        ListNode* n=head;
+        int len=1;
+        while(true){
+            if(n->next==nullptr){
+                n->next=head;
+                break;
+            }
+            len++;
+            n=n->next;
+        }
+        n=head;
+        k%=len;k=len-k;
+        ListNode* pre=n;
+        while(k--){
+            pre=n;
+            n=n->next;
+        }
+        pre->next=nullptr;
         return n;
     }   
 };
